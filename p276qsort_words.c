6de38d00b704ea783763c831fac80d14abc0c897
MarlonZHU <wenfeng.zhu@uqconnect.edu.au>
2019-12-28 17:53:51 +1000

    qsort string

---
+++ p276qsort_words.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+
+int compare_str(char *str1, char *str2);
+
+int main()
+{
+    
+
+    return 0;
+}
+int compare_str(char *str1, char *str2){
+    return strcmp(*(char **str1), *(char **str2));
+}
