899565cd43b032e2616c2c19d9c8018f0fd7a1d5
MarlonZHU <wenfeng.zhu@uqconnect.edu.au>
2019-12-28 11:20:50 +1000

    function pointer

---
+++ p272tabulate.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+
+void tabulate(double (*f)(double), double first, double last, double incr);
+int main(){
+    double final, increment, initial;
+
+    printf("Enter increment: ");
+    scanf("%lf", &increment);
+
+    printf("Enter initial: ");
+    scanf("%lf", &initial);
+    
+    printf("Enter final: ");
+    scanf("%lf", &final);
+
+    printf("\n\tx\tcos(x)\n");
+    tabulate(cos, initial, final, increment);
+
+    printf("\n\tx\tcos(x)\n");
+    return 0;
+}
+
+
+void tabulate(double (*f)(double), double first, double last, double incr)
+{
+    for (; first<=last; first+=incr)
+        printf("%f: %f\n",first, f(first));
+}
+
